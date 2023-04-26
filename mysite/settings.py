b68d888b9af11d37d47aed4815cdc7ffa334e234
mayureshinde <mayureshshinde0495@gmail.com>
2023-04-26 22:19:04 +0530

    Smaller Changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1*wb$$77ueo51r$##6tomcvx!$twdbame95=(wr(eizz8&oyf8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mayuresh.pythonanywhere.com',]
+ALLOWED_HOSTS = ['mayuresh.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
@@ -122,11 +122,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Added manually
-#STATICFILES_DIRS = [
-#   os.path.join(BASE_DIR, "static"),
-#]
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, "static"),
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
